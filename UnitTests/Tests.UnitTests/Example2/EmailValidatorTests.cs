8b02db904cb94b3030f633c95e67e1c4afce2bc3
alirezadoostdar <alireza.doostdar@gmail.com>
2024-10-23 13:36:37 +0200

    complete example2 email validator

---
+++ UnitTests/Tests.UnitTests/Example2/EmailValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Tests.Src.UnitTests.Example2;
+
+namespace Tests.UnitTests.Example2;
+
+public class EmailValidatorTests
+{
+    [Theory]
+    [InlineData("alireza.doostdar@gmail.com",true)]
+    [InlineData("",false)]
+    [InlineData(null,false)]
+    [InlineData("b@sum.com",true)]
+    public void IsEmailValid_ShouldReturnExpectResult(string email,bool expectResult)
+    {
+        var sut = new EmailValidator();
+
+        var result = sut.IsValidEmail(email);
+
+        result.Should().Be(expectResult);
+    }
+}
