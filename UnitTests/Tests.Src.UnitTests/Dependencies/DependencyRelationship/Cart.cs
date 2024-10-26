f009fe2ac5ea154c8d8978a98459122e6f789f5e
alirezadoostdar <alireza.doostdar@gmail.com>
2024-10-26 23:43:56 +0200

    Add Dependency relation test

---
+++ UnitTests/Tests.Src.UnitTests/Dependencies/DependencyRelationship/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Src.UnitTests.Dependencies.DependencyRelationship;
+
+public class Cart
+{
+    private UserAuthentication UserAuthentication;
+    public Cart(UserAuthentication userAuthentication)
+    {
+        UserAuthentication = userAuthentication;
+    }
+
+    public bool AddItemToCart(string item)
+    {
+        if (!UserAuthentication.IsAuthenticated)
+        {
+            throw new InvalidOperationException("User must be logged in to the add item to the cart!");
+        }
+
+        //your logic
+        return true;
+    }
+}
---
+++ UnitTests/Tests.UnitTests/Dependencies/DependencyRelationship/CatrTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Src.UnitTests.Dependencies.DependencyRelationship;
+
+namespace Tests.UnitTests.Dependencies.DependencyRelationship;
+
+public class CatrTests
+{
+    [Fact]
+    public void AddItemToCart_ShouldFail_IfNotLoggedin()
+    {
+        var auth = new UserAuthentication();
+        var cart = new Cart(auth);
+    }
+}
