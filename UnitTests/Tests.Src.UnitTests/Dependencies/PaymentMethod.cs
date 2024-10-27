b39591cbb0d62154466312aea4e10779a7f0d254
alirezadoostdar <alireza.doostdar@gmail.com>
2024-10-27 22:35:25 +0100

    add inheritance test in payment example

---
+++ UnitTests/Tests.Src.UnitTests/Dependencies/PaymentMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Src.UnitTests.Dependencies;
+
+public abstract class PaymentMethod
+{
+    public abstract bool Process(decimal amount);
+}
+
+public  class CreditCartPayment : PaymentMethod
+{
+    public override bool Process(decimal amount)
+    {
+        return true;
+    }
+}
+
+public class PaypalPayment : PaymentMethod
+{
+    public override bool Process(decimal amount)
+    {
+        return true;
+    }
+}
---
+++ UnitTests/Tests.UnitTests/Dependencies/PaymentMethodTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Src.UnitTests.Dependencies;
+
+namespace Tests.UnitTests.Dependencies;
+
+public class PaymentMethodTests
+{
+    [Fact]
+    public void Payment_ShouldProcessCorrectly_WithDiffrentMethod()
+    {
+        //arrange
+        PaymentMethod method1 = new CreditCartPayment();
+        PaymentMethod method2 = new PaypalPayment();
+
+        //assert
+        method1.Process(100m).Should().BeTrue();
+        method2.Process(100m).Should().BeTrue();
+    }
+}
